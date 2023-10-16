ba041747c815c8a623c0481d88597f13d903dc29
Matielo J Geronimo <matielojg@gmail.com>
2023-10-15 22:56:57 -0300

    alter UserModel entity in queries

--- src/main/java/br/com/matielojg/todolist/user/UserModel.java
+++ src/main/java/br/com/matielojg/todolist/user/UserModel.java
@@ -1,10 +1,29 @@
 package br.com.matielojg.todolist.user;
 
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    @Column(name = "usuario")
     public String username;
     public String name;
-    public String password;  
+    public String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    
 }
