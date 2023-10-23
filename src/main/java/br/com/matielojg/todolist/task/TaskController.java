85a5c5c6063c6a6de9eef5e189f2d8ddbac92ec0
Matielo J Geronimo <matielojg@gmail.com>
2023-10-23 00:25:35 -0300

    put method to update tasks partially

--- src/main/java/br/com/matielojg/todolist/task/TaskController.java
+++ src/main/java/br/com/matielojg/todolist/task/TaskController.java
@@ -9,11 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.matielojg.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -51,7 +54,18 @@ public class TaskController {
     @GetMapping("/")
     public List<TaskModel> list(HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
-        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
-        return tasks;
+        return this.taskRepository.findByIdUser((UUID) idUser);
     }
+
+    // http://localhost:8080/tasks/
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel,@PathVariable UUID id, HttpServletRequest request) {
+  
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel,task);
+  
+        return this.taskRepository.save(task);
+    }
+
 }
